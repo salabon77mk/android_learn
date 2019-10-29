aa3510add84dc075e6596afa2429468176f8beef
Mick <salabon77mk@gmail.com>
2019-10-29 12:58:39 -0400

    Completed chap 3

--- 1_quiz/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 1_quiz/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,12 +16,17 @@ import com.example.myapplication.Models.Question;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "QUIZACTIVITY";
+    private static final String KEY_INDEX = "index";
+    private final boolean decr = true;
 
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
     private TextView mQuestionTextView;
+    private boolean[] mAnswered;
+    private int mAnsweredCount;
+    private int mCorrect;
 
     // Should be an adapter??
     private Question[] mQuestionBank = new Question[] {
@@ -35,15 +40,22 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCurrentIndex = 0;
 
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
 
+        mAnswered = new boolean[mQuestionBank.length];
+        mAnsweredCount = 0;
+
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
         updateQuestion();
-
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -61,11 +73,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateIndex(!decr);
                 updateQuestion();
             }
         });
@@ -74,10 +87,27 @@ public class MainActivity extends AppCompatActivity {
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                updateIndex(decr);
                 updateQuestion();
             }
         });
+    }
+
+    // TODO Updates current question index and sets prev question to true
+    private void updateIndex(boolean isDecr){
+        if(isDecr){
+            do {
+                mCurrentIndex--;
+                if(mCurrentIndex < 0){
+                    mCurrentIndex += mQuestionBank.length;
+                }
+            }
+            while (mAnswered[mCurrentIndex]);
+        }
+        else{
+            do{ mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length; }
+            while (mAnswered[mCurrentIndex]);
+        }
     }
 
     private void updateQuestion(){
@@ -86,18 +116,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void checkAnswer(boolean userPressedTrue){
-        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        if(mAnswered[mCurrentIndex]){
+            Toast.makeText(this, R.string.already_answered, Toast.LENGTH_SHORT).show();
+            return;
+        }
 
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
         int messageResId = 0;
 
         if(userPressedTrue == answerIsTrue){
             messageResId = R.string.correct_toast;
+            mCorrect++;
         }
         else{
             messageResId = R.string.incorrect_toast;
         }
+        mAnswered[mCurrentIndex] = true;
+        mAnsweredCount++;
 
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+        Toast t = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT);
+        t.setGravity(Gravity.TOP, 0, 0);
+        t.show();
+
+        checkFinish();
+    }
+
+
+    public void checkFinish(){
+        if(mAnsweredCount == mQuestionBank.length) {
+            String result = mCorrect + " / " + mQuestionBank.length;
+            Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+            mAnsweredCount = 0;
+
+            // restart quiz
+            for(int i = 0; i < mAnswered.length; i++)
+                mAnswered[i]= false;
+        }
     }
 
     @Override
@@ -130,5 +184,11 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onDestroy() called");
     }
 
-
+    // saving the index for when device is rotated(onDestroy is called and then onCreate)
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
 }
