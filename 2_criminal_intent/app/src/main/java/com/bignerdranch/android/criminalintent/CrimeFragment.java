7da9cdb4fc3961f168e60163ce1299dac86563fb
Mick <salabon77mk@gmail.com>
2019-11-05 08:46:40 -0500

    Learning about fragments

---
+++ 2_criminal_intent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
+    }
+}
