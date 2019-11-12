1da4da03bcfb18dcbd647bb4b4e2d7a3157a5e51
Mick <salabon77mk@gmail.com>
2019-11-12 13:45:33 -0500

    Completed chapter 8 on recycler views

--- 2_criminal_intent/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ 2_criminal_intent/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -1,23 +1,24 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if(fragment == null){
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
