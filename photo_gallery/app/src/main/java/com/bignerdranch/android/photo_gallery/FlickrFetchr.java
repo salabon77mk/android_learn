7db55c185efe849de9c8b604d267039985876ba7
Mick <salabon77mk@gmail.com>
2019-11-28 09:17:54 -0500

    First photo gallery commit

--- photo_gallery/app/src/main/java/com/bignerdranch/android/photo_gallery/FlickrFetchr.java
+++ photo_gallery/app/src/main/java/com/bignerdranch/android/photo_gallery/FlickrFetchr.java
@@ -1,13 +1,27 @@
 package com.bignerdranch.android.photo_gallery;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FlickrFetchr {
-    static final int BUFFER_SIZE = 1024;
+    private static final String TAG = "flickrfetcher";
+
+    private static final int BUFFER_SIZE = 1024;
+
+    private static final String API_KEY = setApiKey();
 
     public byte[] getUrlBytes(String urlSpec) throws IOException{
         URL url = new URL(urlSpec);
@@ -37,5 +51,23 @@ public class FlickrFetchr {
 
     public String getUrlString(String urlSpec) throws IOException{
         return new String(getUrlBytes(urlSpec));
+    }
+
+    private static String setApiKey() {
+        String file ="/src/main/assets/flickrapikey.txt";
+        String currentLine = "";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            currentLine = reader.readLine();
+            reader.close();
+            return currentLine;
+        }
+        catch (FileNotFoundException fnfe){
+            Log.e(TAG, "FileNotFoundException in setApieKey", fnfe);
+        }
+        catch (IOException ioe){
+            Log.e(TAG, "IOException in setApiKey", ioe);
+        }
+        return currentLine;
     }
 }
