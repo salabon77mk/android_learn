56b1998c40812e51c8ead62d725e1b4b86586c8d
Mick <salabon77mk@gmail.com>
2019-12-11 14:01:46 -0500

    Finished chapter 29, broadcast services

--- photo_gallery/app/src/main/java/com/bignerdranch/android/photo_gallery/PhotoGalleryActivity.java
+++ photo_gallery/app/src/main/java/com/bignerdranch/android/photo_gallery/PhotoGalleryActivity.java
@@ -1,8 +1,16 @@
 package com.bignerdranch.android.photo_gallery;
 
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.fragment.app.Fragment;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    public static Intent newIntent(Context context){
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
+
     @Override
     protected Fragment createFragment() {
         return PhotoGalleryFragment.newInstance();
--- photo_gallery/app/src/main/java/com/bignerdranch/android/photo_gallery/PollService.java
+++ photo_gallery/app/src/main/java/com/bignerdranch/android/photo_gallery/PollService.java
@@ -1,17 +1,93 @@
 package com.bignerdranch.android.photo_gallery;
 
+import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.SystemClock;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class PollService extends IntentService {
+    private static final String CHANNEL_ID = "SearchPoll";
     private static final String TAG = "PollService";
+
+    public static final String ACTION_SHOW_NOTIFICATION =
+            "com.bignerdranch.android.photo_gallery.SHOW_NOTIFICATION";
+    public static final String PERM_PRIVATE =
+            "com.bignerdranch.android.photogallery.PRIVATE";
+    public static final String REQUEST_CODE = "REQUEST_CODE";
+    public static final String NOTIFICATION = "NOTIFICATION";
+
+    // 1 minute interval
+    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(15);
 
     public static Intent newIntent(Context context){
         return new Intent(context, PollService.class);
+    }
+
+    public static void setServiceAlarm(Context context, boolean isOn){
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        if(isOn){
+            alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(),
+                    POLL_INTERVAL_MS, pi);
+        }
+        else{
+            alarmManager.cancel(pi);
+            pi.cancel();
+        }
+
+        QueryPreferences.setAlarmOn(context, isOn);
+    }
+
+    public static boolean isServiceAlarmOn(Context context){
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent
+                .getService(context, 0, i, PendingIntent.FLAG_NO_CREATE);
+
+        return pi != null;
+    }
+
+    private boolean isNetworkAvailableAndConnected(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            if (Build.VERSION.SDK_INT < 23) {
+                final NetworkInfo ni = cm.getActiveNetworkInfo();
+                if (ni != null) {
+                    return (ni.isConnected() &&
+                            (ni.getType() == ConnectivityManager.TYPE_WIFI || ni.getType() == ConnectivityManager.TYPE_MOBILE));
+                }
+            } else {
+                final Network network = cm.getActiveNetwork();
+                if (network != null) {
+                    final NetworkCapabilities nc = cm.getNetworkCapabilities(network);
+                    return (nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                            || nc.hasTransport(NetworkCapabilities.TRANSPORT_WIFI));
+                }
+            }
+        }
+        return false;
     }
 
     public PollService(){
@@ -19,6 +95,72 @@ public class PollService extends IntentService {
     }
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+        if(!isNetworkAvailableAndConnected()){
+            return;
+        }
         Log.i(TAG, "Received an intent: " + intent);
+        String query = QueryPreferences.getStoredQuery(this);
+        String lastResultId = QueryPreferences.getLastResultId(this);
+        List<GalleryItem> items;
+
+        if(query == null){
+            items = new FlickrFetchr().fetchRecentPhotos();
+        }
+        else{
+            items = new FlickrFetchr().searchPhotos(query);
+        }
+
+        if(items.size() == 0){
+            return;
+        }
+
+        String resultId = items.get(0).getId();
+        if(resultId.equals(lastResultId)){
+            Log.i(TAG, "Got an old result: " + resultId);
+        }
+        else{
+            Log.i(TAG, "Got a new result" + resultId);
+
+            Resources resources = getResources();
+            Intent i = PhotoGalleryActivity.newIntent(this);
+            PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+            // Feel like this should be called when the activity boots up
+            oreoNotificationRegister();
+
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setTicker(resources.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_pictures_title))
+                    .setContentText(resources.getString(R.string.new_pictures_text))
+                    .setContentIntent(pi)
+                    .build();
+
+            showBackgroundNotification(0, notification);
+        }
+
+        QueryPreferences.setLastResultId(this, resultId);
+    }
+
+    private void showBackgroundNotification(int requestCode, Notification notification){
+        Intent i = new Intent(ACTION_SHOW_NOTIFICATION);
+        i.putExtra(REQUEST_CODE, requestCode);
+        i.putExtra(NOTIFICATION, notification);
+        sendOrderedBroadcast(i, PERM_PRIVATE, null, null,
+                Activity.RESULT_OK, null, null);
+    }
+
+    private void oreoNotificationRegister(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Resources resources = getResources();
+            CharSequence name = resources.getString(R.string.channel_name_search_results);
+            String description = resources.getString(R.string.channel_description_search_results);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
